38ad86b8e0c59558441de4b0dd39a68fa831d671
madnir12 <anfz1200@gmail.com>
2022-10-13 20:20:19 +0500

    just for test

---
+++ ast/scripts/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArPQ7LLbxPlV57sbgXx6scNLCcpZXzR5Y",
+  authDomain: "madaris-site-template.firebaseapp.com",
+  projectId: "madaris-site-template",
+  storageBucket: "madaris-site-template.appspot.com",
+  messagingSenderId: "167296326570",
+  appId: "1:167296326570:web:1ea409f573efa8ef41e9d3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
